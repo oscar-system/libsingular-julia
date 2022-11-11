235b29b6030ef1132f157b6faf27d1be7889e2c3
dan <tthsqe12@gmail.com>
2022-11-11 16:06:56 +0100

    Don't corrupt the ring data. Also, incidentally use lex.

--- coeffs.cpp
+++ coeffs.cpp
@@ -7,8 +7,7 @@ auto transExt_helper(coeffs cf, jlcxx::ArrayRef<uint8_t *> param)
     for (int i = 0; i < len; i++) {
         param_ptr[i] = reinterpret_cast<char *>(param[i]);
     }
-    ring r = rDefault(cf, len, param_ptr);
-    r->order[0] = ringorder_dp;
+    ring r = rDefault(cf, len, param_ptr, ringorder_lp);
     delete[] param_ptr;
     TransExtInfo extParam;
     extParam.r = r;
